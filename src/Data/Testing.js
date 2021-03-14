32924389342c41568dd0c57e37803a201bca7812
liconl <60866363+liconl@users.noreply.github.com>
2021-03-14 00:47:57 -0800

    Removed testing files

--- src/Data/Testing.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useStateValue } from "../Context/StateProvider";
-
-function Testing() {
-  const [{ symbol }, dispatch] = useStateValue();
-  return <div></div>;
-}
-
-export default Testing;
